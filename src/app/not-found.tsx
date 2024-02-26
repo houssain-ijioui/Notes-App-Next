d9af5f63ccfd7bdda08debeec43673ea70b305d5
Lahoucine Ijioui <ije1work@gmail.com>
2024-02-26 15:41:03 +0100

    create page files and not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function notFound() {
+    return (
+        <h1 className="text-center">Page Not Found</h1>
+    )
+}
---
+++ src/app/note/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Note() {
+    return (
+        <h1 className="text-center">Note Page</h1>
+    )
+}
